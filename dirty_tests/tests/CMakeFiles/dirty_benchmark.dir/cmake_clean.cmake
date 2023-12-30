ab38e8189125d8989dfd984d81bc9b106bb2d587
Jere <Luhtanen.jere@gmail.com>
2023-12-30 12:45:14 +0200

    Added cmake and updated readme.

---
+++ dirty_tests/tests/CMakeFiles/dirty_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dirty_benchmark.dir/benchmark_main.cpp.o"
+  "CMakeFiles/dirty_benchmark.dir/benchmark_main.cpp.o.d"
+  "dirty_benchmark"
+  "dirty_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dirty_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dirty_tests/tests/reserveTest.cpp
@@ -0,0 +1,58 @@
+#include <benchmark/benchmark.h>
+
+#include "ring_buffer.hpp"
+#include <vector>
+
+ring_buffer<size_t> small_buffer(1, 1);
+std::vector<size_t> small_vec(1, 1);
+
+static void BM_reserve_small_buffer(benchmark::State& state)
+{
+    for(auto _ : state)
+    {
+        for(auto i = state.range(0) ; i < state.range(0) + 1000 ; i++)
+        {
+            if(i % 2 == 0)
+            small_buffer.reserve(i);
+        }
+    }
+}
+
+static void BM_reserve_small_vector(benchmark::State& state)
+{
+    for(auto _ : state)
+    {
+        for(auto i = state.range(0) ; i < state.range(0) + 1000 ; i++)
+        {
+            if(i % 2 == 0)
+            small_vec.reserve(i);
+        }
+    }
+}
+
+// static void BM_reserve_medium_buffer(benchmark::State& state)
+// {
+//     for(auto _ : state)
+//     {
+//         for(auto i = 0; i < ; i++)
+//         {
+//             medium_buffer.reserve(state.range(0));
+//         }
+//     }
+// }
+
+// static void BM_reserve_medium_vector(benchmark::State& state)
+// {
+//     for(auto _ : state)
+//     {
+//         for(auto i = ; i < state.range(0); i++)
+//         {
+//             medium_vec.reserve(i);
+//         }
+//     }
+// }
+
+BENCHMARK(BM_reserve_small_buffer)->Range(2048, 1 << 19);
+BENCHMARK(BM_reserve_small_vector)->Range(2048, 1 << 19);
+// BENCHMARK(BM_reserve_medium_buffer)->Range(4096, 1 << 13);
+// BENCHMARK(BM_reserve_medium_vector)->Range(4096, 1 << 13);
